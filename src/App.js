1a58cd058f5a3f0edf502d3238f5665d3f6a582a
himan shavaleh <88880060+himan-shavaleh@users.noreply.github.com>
2022-08-27 13:21:50 +0430

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import "./App.css";
+import people from "./data";
+import { useEffect, useState } from "react";
 function App() {
+  const [personIndex, setPersonIndex] = useState(0);
+  const nextSlideHandler = () => {
+    setPersonIndex((prevIndex) => {
+      let index = prevIndex + 1;
+      if (prevIndex >= people.length - 1) {
+        index = 0;
+      }
+      return index;
+    });
+  };
+  const prevSlideHandler = () => {
+    setPersonIndex((prevIndex) => {
+      let index = prevIndex - 1;
+      if (prevIndex <= 0) {
+        index = people.length - 1;
+      }
+      return index;
+    });
+  };
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setPersonIndex((prevIndex) => {
+        let index = prevIndex + 1;
+        if (prevIndex >= people.length - 1) {
+          index = 0;
+        }
+        return index;
+      });
+    }, 3000);
+    return () => clearInterval(timer);
+  }, [personIndex]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div className="title">
+        <h1>
+          <span>/</span>reviews
+        </h1>
+      </div>
+      <div className="section-center">
+        {people.map((peop, index) => {
+          let stateMent = "nextSlide";
+          if (personIndex === people.length - 1) {
+            stateMent = "lastSlide";
+          }
+          if (personIndex === index) {
+            stateMent = "activeSlide";
+          }
+          const { id, name, title, image, quote } = peop;
+
+          return (
+            <article key={id} className={stateMent}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <h4 className="title">{title}</h4>
+              <p className="text">{quote}</p>
+            </article>
+          );
+        })}
+        <button className="next" onClick={nextSlideHandler}>
+          <FaChevronRight className="icon" />
+        </button>
+        <button className="prev" onClick={prevSlideHandler}>
+          <FaChevronLeft className="icon" />
+        </button>
+      </div>
+    </section>
   );
 }
 
